dfa98cc8873091c824fbfdc55f27742d2ef19dfd
Fene-87 <fenekayas87@gmail.com>
2024-02-09 18:04:12 +0300

    Create a responsive navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,14 +1,24 @@
+import { faBars, faX } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 const Navbar = () => {
     return (
-        <div>
-            <h1>Mark Fenekayas</h1>
-            <ul>
+        <div className="flex fixed w-full justify-between bg-[#3e3c3c] text-white mx-auto p-5 shadow">
+            <h1 className="text-[22px] cursor-pointer">Mark Fenekayas</h1>
+            <ul className="hidden md:flex gap-10">
+                <li className="cursor-pointer hover:text-[#d3d3d3]">Portfolio</li>
+                <li className="cursor-pointer hover:text-[#d3d3d3]">About</li>
+                <li className="cursor-pointer hover:text-[#d3d3d3]">Blog</li>
+                <li className="cursor-pointer hover:text-[#d3d3d3]">Contact</li>
+            </ul>
+            <FontAwesomeIcon icon={faBars} className="cursor-pointer md:hidden" />
+            <ul className="md:hidden">
+                <li><FontAwesomeIcon icon={faX} /></li>
                 <li>Portfolio</li>
                 <li>About</li>
-                <li>Contact</li>
                 <li>Blog</li>
+                <li>Contact</li>
             </ul>
         </div>
     )
