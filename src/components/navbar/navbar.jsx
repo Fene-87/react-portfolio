e53178b4dd1391f1393df2221175bc5c6d98aea7
Fene-87 <fenekayas87@gmail.com>
2024-02-09 23:37:21 +0300

    Style navbar and make it fully responsive

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,25 +1,34 @@
 import { faBars, faX } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useState } from "react";
 
 const Navbar = () => {
+    const [closedMenu, setClosedMenu] = useState(false);
+    const toggleMenu = () => {
+        setClosedMenu(!closedMenu)
+    }
+
     return (
-        <div className="flex fixed w-full justify-between bg-[#3e3c3c] text-white mx-auto p-5 shadow">
-            <h1 className="text-[22px] cursor-pointer">Mark Fenekayas</h1>
-            <ul className="hidden md:flex gap-10">
+        <div className="md:flex fixed w-full items-center justify-between bg-[#3e3c3c] text-white mx-auto shadow-md">
+            <div className="flex items-center justify-between bg-[#3e3c3c] p-5">
+              <h1 className="text-[22px] cursor-pointer">Mark Fenekayas</h1>
+              <FontAwesomeIcon icon={closedMenu ? faX : faBars} onClick={toggleMenu} className="cursor-pointer md:hidden" />
+            </div>
+            
+            <ul className={`h-[100vh] md:h-[3vh] md:flex z-[-1] bg-[#3e3c3c] md:z-auto absolute md:static items-center gap-10 top-[-400px] transition-all
+                ease-in duration-500 w-full md:w-auto left-0 p-5 ${closedMenu ? 'top-[4.8rem]' : 'top-[-700px]'}`}>
                 <li className="cursor-pointer hover:text-[#d3d3d3]">Portfolio</li>
-                <li className="cursor-pointer hover:text-[#d3d3d3]">About</li>
-                <li className="cursor-pointer hover:text-[#d3d3d3]">Blog</li>
-                <li className="cursor-pointer hover:text-[#d3d3d3]">Contact</li>
+                <li className="cursor-pointer hover:text-[#d3d3d3] my-10 md:my-0">About</li>
+                <li className="cursor-pointer hover:text-[#d3d3d3] my-10 md:my-0">Blog</li>
+                <li className="cursor-pointer hover:text-[#d3d3d3] my-10 md:my-0">Contact</li>
             </ul>
-            <FontAwesomeIcon icon={faBars} className="cursor-pointer md:hidden" />
-            <ul className="md:hidden">
-                <li><FontAwesomeIcon icon={faX} /></li>
-                <li>Portfolio</li>
+            
+            {/* <ul className="md:hidden">
+                <li className="border-[#d3d3d3]">Portfolio</li>
                 <li>About</li>
                 <li>Blog</li>
                 <li>Contact</li>
-            </ul>
+            </ul> */}
         </div>
     )
 };
