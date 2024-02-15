1cdcef9d79bec1bbc0d27ff05adea4c2738f6f3c
Fene-87 <fenekayas87@gmail.com>
2024-02-15 23:18:53 +0300

    Structure the projects section

---
+++ src/components/projects/projects.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Projects = () => {
+    return (
+        <div>
+            <div>
+                <h2>My Recent Works</h2>
+                <div></div>
+            </div>
+
+            <div>
+                <div>
+                    <h3>Multi-Post stories</h3>
+                    <p>
+                        Below you'll find an impressive line-up of my remarkable projects,
+                        each a testament to my remarkable passion and expertise. Join me
+                        as we explore this diverse array of projects, where artistry meets
+                        functionality, and innovation knows no bounds.
+                    </p>
+                </div>
+            </div>
+
+            <div></div>
+        </div>
+    )
+}
+
+export default Projects;
