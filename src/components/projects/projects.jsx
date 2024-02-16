897569f70da766931958132d96cb7268f1d8306a
Fene-87 <fenekayas87@gmail.com>
2024-02-16 20:22:42 +0300

    Style and make section header responsive

--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 const Projects = () => {
     return (
-        <div>
-            <div>
-                <h2>My Recent Works</h2>
-                <div></div>
+        <div className="h-full w-full p-5 pt-20 md:p-20">
+            <div className="flex flex-col md:flex-row gap-10 md:gap-5 mt-0 md:mt-10">
+                <h2 className="text-[#091e42] text-center md:text-left font-medium text-[32px] md:text-[40px] max-w-full md:min-w-[320px]">My Recent Works</h2>
+                <div>
+                   <hr class="w-48 md:w-[60rem] h-1 mx-auto my-4 bg-gray-400 border-0 rounded md:my-10 dark:bg-gray-900"></hr>
+                </div>
             </div>
 
             <div>
