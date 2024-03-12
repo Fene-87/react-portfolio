c88013d190f844b3a77376646a4816e0071e31ea
Fene-87 <fenekayas87@gmail.com>
2024-03-12 15:28:14 +0300

    Implement contact-section

---
+++ src/components/Contact.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { useForm } from "react-hook-form"
+
+const Contact = () => {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+
+  const onSubmit = () => {
+
+  }
+
+  return (
+    <div className='h-full w-full flex flex-col md:flex-row p-5 md:p-20 gap-20 items-start md:items-center justify-center'>
+      <div>
+        <p className='text-[#091e42] w-[455px] text-4xl leading-[52px] font-bold text-center md:text-start'>
+            I'm always interested in hearing about new projects, so
+            if you'd like to chat please get in touch.
+        </p>
+      </div>
+
+      <form 
+        action="" 
+        className='flex flex-col'
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <label className='hidden' htmlFor="full_name">Full name</label>
+        <input 
+          type="text" 
+          name="full_name" 
+          id="full_name"
+          {...register("fullName", {
+            required: {
+              value: true,
+              message: "Full name cannot be blank",
+            }
+          })}
+          placeholder='Full name'
+          className='w-[327px] h-[48px] text-[15px] md:w-[652px] border-b-2 border-dotted border-[#dbd8d7] outline-none'
+        />
+
+        <label className='hidden' htmlFor="email">Email address</label>
+        <input
+          type="email" 
+          name="email" 
+          id="email" 
+          {...register("email", {
+            required: {
+              value: true,
+              message: "Email cannot be blank",
+            }
+          })}
+          placeholder='Email address'
+          className='w-[327px] h-[48px] text-[15px] md:w-[652px] border-b-2 border-dotted border-[#dbd8d7] outline-none'
+        />
+
+        <textarea
+          name="message" 
+          id="message"
+          {...register("message", {
+            required: {
+              value: true,
+              message: "Message cannot be blank",
+            }
+          })}
+          placeholder='Your message...'
+          className='w-[327px] h-[114px] text-[15px] md:w-[652px] bg-[#fbf8f7] pt-2 mt-5 outline-none'
+        ></textarea>
+        <button type='submit' className='w-[131px] h-[48px] bg-[#ff6b00] hover:bg-[#091e42] text-white text-[17px] font-bold leading-[24px] mt-5 rounded-md'>Get in touch</button>
+      </form>
+    </div>
+  )
+}
+
+export default Contact;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from "../components/navbar/navbar";
 import Info from "../components/info/info";
 import Projects from "../components/projects/projects";
 import About from "../components/About";
+import Contact from "../components/Contact";
 
 const Home = () => {
     return (
@@ -11,6 +12,7 @@ const Home = () => {
           <Info />
           <Projects />
           <About />
+          <Contact />
         </div>
     )
 }
